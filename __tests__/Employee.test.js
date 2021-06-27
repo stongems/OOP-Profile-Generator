a52ba93530ecd48e2854eb351864fcadde6808b5
St.Onge <stonge.ms@gmail.com>
2021-06-26 23:08:43 -0700

    added test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  describe("Initilization", () => {
+    it("should create an object containing employee name, email and id", () => {
+      //Arrange
+      const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
+      //Assert
+      expect("name" in employee).toBe(true);
+      expect("email" in employee).toBe(true);
+      expect("id" in employee).toBe(true);
+    });
+
+    it("should set the name property of the employee", () => {
+      //Arrange
+      const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
+      //Assert
+      expect(employee.name).toBe("Jane");
+    });
+
+    it("should set the email property of the employee", () => {
+      //Arrange
+      const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
+      //Assert
+      expect(employee.email).toBe("Janedoe@gmail.com");
+    });
+
+    it("should set the ID property of the employee", () => {
+      //Arrange
+      const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
+      //Assert
+      expect(employee.id).toBe(820);
+      
+    });
+  });
+});
