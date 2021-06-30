570c36ed96e9d63aee02e21f4397ce8c3a05cfe7
St.Onge <stonge.ms@gmail.com>
2021-06-29 19:25:18 -0700

    finished notes in test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,32 +3,32 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
   describe("Initilization", () => {
     it("should create an object containing employee name, email and id", () => {
-      //Arrange
+      //================================================== Arrange
       const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
-      //Assert
+      //=================================================== Assert
       expect("name" in employee).toBe(true);
       expect("email" in employee).toBe(true);
       expect("id" in employee).toBe(true);
     });
 
     it("should set the name property of the employee", () => {
-      //Arrange
+      //================================================== Arrange
       const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
-      //Assert
+      //=================================================== Assert
       expect(employee.name).toBe("Jane");
     });
 
     it("should set the email property of the employee", () => {
-      //Arrange
+      //================================================== Arrange
       const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
-      //Assert
+      //=================================================== Assert
       expect(employee.email).toBe("Janedoe@gmail.com");
     });
 
     it("should set the ID property of the employee", () => {
-      //Arrange
+      //================================================== Arrange
       const employee = new Employee("Jane", "Janedoe@gmail.com", 820);
-      //Assert
+      //=================================================== Assert
       expect(employee.id).toBe(820);
       
     });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -15,28 +15,29 @@ describe("Engineer", () => {
     it("should set the name property of the engineer", () => {
       //Arrange
       const engineer = new Engineer("John", "Johndoe@gmail.com", 314, "jd516");
-      //Assert
+      //=================================================== Assert
       expect(engineer.name).toBe("John");
     });
 
     it("should set the email property of the engineer", () => {
-      //Arrange
+      //================================================== Arrange
       const engineer = new Engineer("John", "Johndoe@gmail.com", 314, "jd516");
-      //Assert
+      //=================================================== Assert
       expect(engineer.email).toBe("Johndoe@gmail.com");
     });
 
     it("should set the ID property of the engineer", () => {
-      //Arrange
+      //================================================== Arrange
       const engineer = new Engineer("John", "Johndoe@gmail.com", 314, "jd516");
-      //Assert
+      //=================================================== Assert
       expect(engineer.id).toBe(314);
     });
 
     it("should set the github property of the engineer", () => {
-        //Arrange
+        //================================================== Arrange
+      
         const engineer = new Engineer("John", "Johndoe@gmail.com", 314, "jd516");
-        //Assert
+        //=================================================== Assert
         expect(engineer.github).toBe("jd516");
     });
   });
